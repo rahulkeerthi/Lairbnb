da6e88709f2135cb440ae53edd1c2c049520f6cc
Tomas Kudlac <58271990+TomasKudlac@users.noreply.github.com>
2020-02-25 11:14:27 +0000

    Revert "Lair control"

--- app/models/lair.rb
+++ app/models/lair.rb
@@ -1,6 +1,6 @@
 class Lair < ApplicationRecord
   belongs_to :user
-  has_many :bookings, dependent: :destroy
+  has_many :bookings
 
   validates :title, presence: true
   validates :location, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'lairs/show'
-  get 'lairs/new'
-  get 'lairs/create'
-  get 'lairs/edit'
-  get 'lairs/update'
-  get 'lairs/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :users, only: :show

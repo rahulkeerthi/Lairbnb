081310b5d372b00ad1225db407c9040d0be330b1
Tomas Kudlac <kudlac.tomas@gmail.com>
2020-02-27 11:50:51 +0000

    pg search done for both index and home

--- app/models/lair.rb
+++ app/models/lair.rb
@@ -8,4 +8,11 @@ class Lair < ApplicationRecord
   validates :price_per_night, presence: true, numericality: { greater_than: 0 }
   validates :is_hero_lair, inclusion: { in: [true, false] }
   validates :user_id, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_location,
+    against: [ :title, :location ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end

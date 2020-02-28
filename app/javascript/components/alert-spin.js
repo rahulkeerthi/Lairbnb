4d87e0778e7f51aaa223cae5ef964a565c62095b
Shane Wickramasuriya <shane.geeky@gmail.com>
2020-02-28 14:26:00 +0000

    Wokring with sound

--- app/javascript/components/alert-spin.js
+++ app/javascript/components/alert-spin.js
@@ -3,8 +3,10 @@ export const alertSpin = () => {
   document.querySelectorAll(".lair-card-disabled").forEach((card) => {
     card.addEventListener("click", (event) => {
       const node = event.currentTarget.childNodes[1];
-      console.log(node);
+      const audio = new Audio()
+      audio.src = document.getElementById("audio_source").dataset.src
       node.setAttribute("style", "animation-play-state: running;");
+      audio.play()
     });
   });
 }
